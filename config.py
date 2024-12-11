d40a9494afc207de26353350f8986e4c4973f363
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-12-11 21:30:38 +0530

    ♡ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "24996524"
+# -------------------------------------------------------------
+API_HASH = "b0825fc3f523fd28b2ea1735a07a058e"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING1 = getenv("STRING_SESSION", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "6260080241"))
+SUPPORT_GRP = "ANJALIWORLD"
+UPDATE_CHNL = "ANJALINETWORK"
+OWNER_USERNAME = "AnjaliOwnerBot"
