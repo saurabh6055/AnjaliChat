bbd0d7f6f8dd4651445aedfe6e9d787080d3ac35
saurabh6055 <153442157+saurabh6055@users.noreply.github.com>
2025-07-29 20:24:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24996524"
+API_ID = "28199593"
 # -------------------------------------------------------------
-API_HASH = "b0825fc3f523fd28b2ea1735a07a058e"
+API_HASH = "b76f22d4916774d1942cb478323249e3"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6260080241"))
+BOT_TOKEN = getenv("8257694658:AAEPG083zze5Mh0VzMcUVGVo4Mfjopc1tZs", None)
+STRING1 = getenv("BQE1hZwAAua08AlAs3pyhs8Mk9c_UGH3Rq3VwYE52NhLgA5tw5_ao2foVJXJ-DsO_mmi-xCfGfowJKaRI6evuXjEMl4DTc0xbeFTnVVW1mXhgUjqGRsZrG3AaSCOcEKIfM7kyGVtVNq3fhBtWTnETG4sfItNMplPFPWxGaAaJHa-gsrx1mUAQlPJ-LKTvGpJKbp_aKOEnNN-yMdUxjEsMYnuuTdFnkIB6jiYkAU21dw9ffm1pB49YiaqhxW1kbWqXmGlqNc19IxW3M1Ym_yWrn61uK9vGdpQjgJLCLWkacJrPCCn8N7Zr902aVy47qkQ1EADBmyT--q9pjakXRvUFO4CLmB7dAAAAAHSzqIUAA", None)
+MONGO_URL = getenv("mongodb+srv://deepu_user:deepu_pass123@cluster0.rhkc94l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+OWNER_ID = int(getenv("OWNER_ID", "7831724564"))
 SUPPORT_GRP = "ANJALIWORLD"
 UPDATE_CHNL = "ANJALINETWORK"
-OWNER_USERNAME = "AnjaliOwnerBot"
+OWNER_USERNAME = "Chatz_zbot"
